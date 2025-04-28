88ed56a97e408c7383a0998dda66166012ce689c
LT4-LT20 <158325478+LT4-LT20@users.noreply.github.com>
2025-04-28 14:36:54 -0500

    Update uv.config.js

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -16,12 +16,12 @@
 //  ⠀⠀⠀⠀⠀⠀⠐⢫⠏⢸⠁⠀⠈⢳⠘⢧⣙⢦⠀⠀⠀
 
 self.__uv$config = {
-  prefix: "/active/uv/service/",
+  prefix: "/UV-Static-2.0/active/uv/service/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
-  handler: "/active/uv/uv.handler.js",
-  client: "/active/uv/uv.client.js",
-  bundle: "/active/uv/uv.bundle.js",
-  config: "/active/uv/uv.config.js",
-  sw: "/active/uv/uv.sw.js",
+  handler: "/UV-Static-2.0/active/uv/uv.handler.js",
+  client: "/UV-Static-2.0/active/uv/uv.client.js",
+  bundle: "/UV-Static-2.0/active/uv/uv.bundle.js",
+  config: "/UV-Static-2.0/active/uv/uv.config.js",
+  sw: "/UV-Static-2.0/active/uv/uv.sw.js",
 };

7b98649acc670c2f7325537f0a8e372996e2f6f4
LT4-LT20 <158325478+LT4-LT20@users.noreply.github.com>
2025-04-28 14:41:12 -0500

    Update register-sw.mjs

--- active/prxy/register-sw.mjs
+++ active/prxy/register-sw.mjs
@@ -2,7 +2,7 @@
 /**
  * Distributed with Ultraviolet and compatible with most configurations.
  */
-const stockSW = "/active/uv/sw.js";
+const stockSW = "/UV-Static-2.0/active/uv/sw.js";
 
 /**
  * List of hostnames that are allowed to run serviceworkers on http://
